c27fd685c6c69f46bb5c104e6973f4d49264d894
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2024-11-21 01:12:07 +0700

    feat: set page titie

--- components/chatbot_app.py
+++ components/chatbot_app.py
@@ -2,28 +2,31 @@ import streamlit as st
 from services.conversation_manager import ConversationManager
 from util.get_instance_id import get_instance_id
 
-class Chatbot:
-    def __init__(self):
-        self.instance_id = get_instance_id()
 
-    def GenerateUI(self) :
-    
+class Chatbot:
+    def __init__(self, page_title="TemanTenang | Tim 7 CendekiAwan"):
+        self.instance_id = get_instance_id()
+        self.page_title = page_title
+
+    def GenerateUI(self):
+        st.set_page_config(page_title=self.page_title)
+
         ### Streamlit code ###
-        st.title("AI Chatbot")
+        st.title("TemanTenang")
 
         # # Display EC2 Instance ID
         st.write(f"**EC2 Instance ID**: {self.instance_id}")
 
         # Initialize the ConversationManager object
-        if 'chat_manager' not in st.session_state:
-            st.session_state['chat_manager'] = ConversationManager()
+        if "chat_manager" not in st.session_state:
+            st.session_state["chat_manager"] = ConversationManager()
 
-        chat_manager = st.session_state['chat_manager']
+        chat_manager = st.session_state["chat_manager"]
 
-        if 'conversation_history' not in st.session_state:
-            st.session_state['conversation_history'] = chat_manager.conversation_history
+        if "conversation_history" not in st.session_state:
+            st.session_state["conversation_history"] = chat_manager.conversation_history
 
-        conversation_history = st.session_state['conversation_history']
+        conversation_history = st.session_state["conversation_history"]
 
         # Chat input from the user
         user_input = st.chat_input("Write a message")
@@ -37,5 +40,3 @@ class Chatbot:
             if message["role"] != "system":
                 with st.chat_message(message["role"]):
                     st.write(message["content"])
-
-    
